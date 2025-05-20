5293571ac2ebaa7fddf06fbd61f917cc63a9c313
rossakyl <rossakayla01@gmail.com>
2025-05-20 10:47:47 +0700

    Implement case 1: prompt and push value onto stack

--- stackusinglingkedList.cpp
+++ stackusinglingkedList.cpp
@@ -93,6 +93,10 @@ int main()
 
         switch (choice)
         {
-        
+            case 1:
+                cout << "Enter the value to push : ";
+                cin >> value;
+                stact.push(value);
+                break;
         }
 }
