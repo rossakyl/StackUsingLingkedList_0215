5c582d8122d3930ad3222323fc1de04403b122d6
rossakyl <rossakayla01@gmail.com>
2025-05-20 10:07:16 +0700

    Implement Stack::push and add empty check in pop()

--- stackusinglingkedList.cpp
+++ stackusinglingkedList.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Node
 {
-public:
+public:  // akses modifier
     int data;
     Node *next;
 
@@ -29,7 +29,21 @@ public:
 
     int push(int value)
     {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push Value: " << value << endl;
+        return value;
+    }
 
+    void pop()
+    {
+        if (isEmpty())
+        {
+
+            cout << "Stack is Empty" << endl;
+        }
     }
 
 };
